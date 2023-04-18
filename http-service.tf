5b78800c00dd22f4bfd2e9bfcf2160f5c69e697d
unknown <jebin90@gmail.com>
2023-04-18 14:48:45 +0530

    updating tf

--- http-service.tf
+++ http-service.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
+      version = "2.11.0"
     }
   }
 }
@@ -11,7 +11,8 @@ terraform {
 # Configure the Kubernetes provider with the Minikube context
 provider "kubernetes" {
     config_context_auth_info = "minikube"
-    config_context_cluster = "minikube"
+    config_path = "C:\Users\User\.kube\config"
+    config_context = "minikube"
 }
 
 # Deploy the application with a Kubernetes Deployment
